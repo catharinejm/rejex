98cb0a20986df72bffb6896c9fecf0f5e0038508
Jon Distad <jon.distad@gmail.com>
2011-09-02 16:15:03 -0400

    Maybe I'll use an NFA?

--- rejex.c
+++ rejex.c
@@ -3,8 +3,56 @@
 #include <string.h>
 #include <stdbool.h>
 
+struct State;
+struct Transition;
+
+struct Transition {
+  char literal;
+  enum Transition_type type;
+  struct State * next_state;
+};
+
+struct State {
+  struct Transition transitions[10];
+  int t_count;
+};
+
+enum Transition_type {
+  LITERAL,
+  ANY_CHAR,
+  LAMBDA
+};
+
+const struct State END_STATE;
+
+void add_transition(struct State * cur_state, struct State * next_state, enum Transition_type ttype, char literal) {
+  struct Transition &new_trans = cur_state->transitions[cur_state->t_count];
+  new_trans.literal = literal;
+  new_trans.type = ttype;
+  new_trans.next_state = next_state;
+  cur_state->t_count++;
+}
+
 bool is_match(char chr, char *m_cur) {
   return *m_cur == '.' || *m_cur == chr;
+}
+
+// consume regex on match
+// consume input on miss
+// is escaped
+
+struct state * build_NFA(char *regex) {
+  struct state * start = malloc(sizeof(struct State));
+  memset(start, 0, sizeof(struct State));
+
+  struct state * cur_state = start;
+  char *cur_tok;
+  bool escaped = false, star = false;
+  for (cur_tok = regex; cur_tok != '\0'; ++cur_tok) {
+    switch (*cur_tok) {
+      case '.': add_transition(cur_state
+    }
+  }
 }
 
 int find_match_index(char *string, char *match) {
@@ -16,7 +64,7 @@ int find_match_index(char *string, char *match) {
       *s_cur != '\0' && !matched;
       ++s_cur) {
 
-  if (is_match(*s_cur, m_cur)) {
+    if (is_match(*s_cur, m_cur)) {
       if (!in_match) {
         m_idx = s_cur - string;
         in_match = true;
